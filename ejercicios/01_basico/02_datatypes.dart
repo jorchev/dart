d8640948f21add1fa40497675895dd0e9f12ea55
Jorge Vega <j.vega@mafasa.org>
2023-01-24 07:41:26 -0700

    02 - Tipado de datos: números

---
+++ ejercicios/01_basico/02_datatypes.dart
@@ -0,0 +1,18 @@
+main() {
+
+  // ===== Números
+  // var a = 10; // declaracion dinamica con var
+  int a = 10;
+  double b = 5.5;
+
+  int? c = null; // ? puede ser nulo
+
+  int _a = 30;
+  double $b = 40;
+
+  double resultado = _a + $b;
+
+  print( c );
+  print( resultado );
+
+}
