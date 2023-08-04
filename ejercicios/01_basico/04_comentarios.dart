87ed55f9ce6fdfc337b9d2474b72e37be0925c32
Jorge Vega <j.vega@mafasa.org>
2023-08-04 16:39:40 -0700

    10 - Comentarios en Dart

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Este archivo tiene por objetivo mostrar los comentarios en Dart
+ * [CreadoEn] : 04/08/2023
+ * 
+ */
+main(){
+
+  // Esta es una variable temporal que quiero definir
+  int a = 10; // A debería de ser 10
+
+  /*   
+  final personas = [
+    // Esta es la primera persona
+    'Fernando', // [0]
+    // Esta es la segunda persona
+    'Pedro', // [1]
+    // Esta es la tercera persona
+    'Carlos' // [2]
+
+  ];
+  */
+
+}
+
+/// Funcion para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar( String nombre, String mensaje ){
+
+}
