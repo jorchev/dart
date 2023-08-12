d4b741cf598b80d4c312cc3ad046ac8944a48e93
Jorge Vega <j.vega@mafasa.org>
2023-08-12 14:03:35 -0700

    29 - Mi solucion a la tarea

---
+++ ejercicios/03_funciones/07_tarea_funciones.dart
@@ -0,0 +1,73 @@
+/** 
+ *  Dado el siguiente código 
+ *    Optimizarlo lo más posible usando funciones
+ *    y todo lo visto en el curso
+ * 
+ *  Ver el primer ejemplo:
+*/
+import 'dart:io';
+import 'dart:math';
+
+main() {
+
+  // Ejemplo: 
+  // Crear una función para imprimir STDOUTS en lugar de 
+  // la siguiente línea. Luego todos los stdouts deberían ser
+  // llamados usando nuestra función personalizada
+  
+  int cont = 1;
+  String resp = 'no';
+
+  do{
+    cargarUsuario(cont);
+    cont++;
+
+    escribir('¿Deseas capturar otro usuario? si/no');
+    resp = leer();
+
+  }while(resp == 'si');
+  
+
+}
+
+void escribir ( dato ){
+  stdout.writeln( dato );
+}
+
+String leer () => stdin.readLineSync() ?? '';
+
+void cargarUsuario(int num){
+
+ escribir('=========== Usuario $num =============');
+
+
+  escribir('¿Cúal es su nombre?');
+  String nombre = leer();
+
+  escribir('¿Qué edad tienes?');
+  String edad = leer();
+  
+  escribir('¿En qué país naciste?');
+  String pais = leer();
+  
+
+  final Map<String, dynamic> usuario = {
+    'nombre': nombre,
+    'edad'  : edad,
+    'pais'  : pais
+  };
+
+  escribir('Usuario $num sin deducciones');
+  escribir( usuario );
+
+  double salario     = (Random().nextDouble() * 950.00 ) + 1000.00;
+  double deducciones = salario * 0.15;
+  double salarioNeto = salario - deducciones;
+
+  usuario['salario']     = salario;
+  usuario['deducciones'] = deducciones;
+  usuario['salarioNeto'] = salarioNeto;
+
+  escribir(usuario);
+
+}
