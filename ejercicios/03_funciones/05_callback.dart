bc18a736d64e2fec6095391cf9e0207ba4bf8db8
Jorge Vega <j.vega@mafasa.org>
2023-08-12 13:00:54 -0700

    27 - Callbacks

---
+++ ejercicios/03_funciones/05_callback.dart
@@ -0,0 +1,20 @@
+void main(List<String> args){
+
+  obtenerUsuario('100' , ( Map persona ){
+
+    print(persona);
+
+  });
+
+}
+
+void obtenerUsuario( String id, Function callback ){
+
+  Map usuario = {
+    'id' : id,
+    'nombre' : 'Juan Carlos'
+  };
+
+  callback( usuario );
+
+}
