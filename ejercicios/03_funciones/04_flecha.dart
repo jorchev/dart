d20dfd37b5fefc87d8dec8db64388420cd122179
Jorge Vega <j.vega@mafasa.org>
2023-08-12 12:51:07 -0700

    26 - Lambda functions o funciones de flecha

---
+++ ejercicios/03_funciones/04_flecha.dart
@@ -0,0 +1,25 @@
+
+
+void main(List<String> args){
+
+  int a = 10, b = 20;
+  int resultado = sumarFlecha( a, b );
+
+  List<int> listado = [1,2,3,4,5,6,6,7,8,9,10,10,1];
+
+  // var nuevoListado = listado.where( (numero) {
+  //   return numero > 4;
+  // } );
+
+  var nuevoListado = listado.where( (n) => n > 4 );
+
+  //print( resultado );
+  print( nuevoListado.toSet() );
+
+}
+
+int sumar( int x, int y) {
+  return x + y;
+}
+
+int sumarFlecha( int x, int y ) => x + y;
