b5ef625b99e9488dca59bad8816dbd8de81b0315
Jorge Vega <jorge.hvr@gmail.com>
2023-08-11 19:06:33 -0700

    25 - Argumentos a las funciones - Por nombre

--- ejercicios/03_funciones/02_argumentos.dart
+++ ejercicios/03_funciones/02_argumentos.dart
@@ -4,8 +4,13 @@ void saludar( String mensaje, [ String nombre = '<insertar nombre>', int edad = 
 
 }
 
+void saludar2(String mensaje, { required String nombre, int veces = 10 }){
+  print('Saludar2: $mensaje $nombre - $veces');
+}
+
 void main(List<String> args){
 
   saludar('Hola', 'Jorge', 37);
+  saludar2('Saludos', veces: 20, nombre: 'Fernando');
 
 }
