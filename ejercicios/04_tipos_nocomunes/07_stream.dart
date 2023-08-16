dc7bd5cdd9c955e49d6c75d373cb24fc33b0f4ea
Jorge Vega <jorge.hvr@gmail.com>
2023-08-15 18:20:16 -0700

    38 - Streams - onError, onDone y cancelOnError

--- ejercicios/04_tipos_nocomunes/07_stream.dart
+++ ejercicios/04_tipos_nocomunes/07_stream.dart
@@ -4,13 +4,22 @@ void main(List<String> args){
 
   final streamController = StreamController();
 
-  streamController.stream.listen((data) {
-    
-    print('Despegando! $data');
-
-   });
+  streamController.stream.listen(
+    (data) => print('Despegando! $data'),
+    onError: (err) => print('Error! $err'),
+    onDone: () => print('Mision completa!'),
+    cancelOnError: false 
+  );
 
    streamController.sink.add('Apollo 11');
+   streamController.sink.add('Apollo 12');
+   streamController.sink.add('Apollo 13');
+   streamController.sink.addError('Houston, Tenemos un problema');
+   streamController.sink.add('Apollo 14');
+   streamController.sink.add('Apollo 15');
+   streamController.sink.add('Apollo 16');
+
+   streamController.sink.close();
 
    print('Fina del main');
 
