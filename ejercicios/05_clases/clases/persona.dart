fbef9a89448dc30ab3d233207be417875d18462a
Jorge Vega <jorge.hvr@gmail.com>
2023-08-16 19:12:28 -0700

    41 - Clases en archivos independientes

---
+++ ejercicios/05_clases/clases/persona.dart
@@ -0,0 +1,19 @@
+class Persona {
+
+  // Campos o propiedades
+  String? nombre;
+  int? edad;
+  String? bio;
+
+  // Gets y sets
+
+
+  // Constructores
+
+
+  // Métodos
+  @override
+  String toString() => 'Nombre: $nombre, Edad: $edad, Bio $bio';
+  
+
+}
