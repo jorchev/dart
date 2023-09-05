7a5f1babd77db70207a84cf89b5808a8c90146c6
Jorge Vega <j.vega@mafasa.org>
2023-09-05 13:07:45 -0700

    57 - Docs: Double - Propiedades y métodos

--- ejercicios/07_docs/01_double.dart
+++ ejercicios/07_docs/01_double.dart
@@ -3,7 +3,23 @@
 void main(List<String> args) {
   
   double numero = 3.1416;
+  double infinito = double.infinity;
 
   print('Firma: ${ numero.sign } :: $numero');
 
+  print('Es finito: ${ numero.isFinite } :: $numero');
+  print('Es finito: ${ infinito.isFinite } :: $infinito');
+
+  print('abs: ${ numero.abs() } :: $numero');
+  print('ceil: ${ numero.ceil() } :: $numero');
+
+  // print('ceil: ${ infinito.ceil() } :: $numero');
+
+  print('ceilToDouble: ${ numero.ceilToDouble() } :: $numero');
+  print('round: ${ numero.round() } :: $numero');
+  print('round: ${ numero.roundToDouble() } :: $numero');
+
+  print('clamp: ${ numero.clamp(1, 3) } :: $numero');
+
 }
+  
