98e11178e076b37c589d0d727ee41e582447008f
Jorge Vega <j.vega@mafasa.org>
2023-09-06 08:50:48 -0700

    59 - Docs: Operadores y más métodos

--- ejercicios/07_docs/02_string.dart
+++ ejercicios/07_docs/02_string.dart
@@ -15,4 +15,18 @@ void main(List<String> args) {
   print('PadLeft: ${ nombreCompleto.padLeft(20,'...') }');
   print('PadRight: ${ nombreCompleto.padRight(20,'...') }');
 
+  print('Operador []: ${ nombreCompleto[0] }');
+  print('Operador *: ${ nombreCompleto * 2 }');
+  print('Operador *: ${ '*' * 10 }');
+
+  print('ReplaceAll: ${ nombreCompleto.replaceAll(new RegExp(r'e'), 'a') }');
+  print('SubString: ${ nombreCompleto.substring(0, 5) }');
+  print('indexOf: ${ nombreCompleto.indexOf(' ') }');
+  
+  print('Split: ${ nombreCompleto.split(' ') }');
+  print('Split: -------${ nombreCompleto.split(' ')[1] }-------');
+
+  // Capitalizar la ultima letra del nombre
+  print('Ejercicio: ${ nombreCompleto[nombreCompleto.length - 1].toUpperCase() }');
+
 }
