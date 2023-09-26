74d4fee116a33efd0464251bde381dc2674c6d1a
Jorge Vega <j.vega@mafasa.org>
2023-09-26 11:32:27 -0700

    Cambios finales

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -2,7 +2,7 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
 
-  paquetes.getReqResServices();
+  // paquetes.getReqResServices();
 
   // Tarea
   paquetes.getReqResPaisServices();
